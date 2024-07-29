f058fd7d6afbf60a7f588d9a78b2623f0bb667a9
dladbfla <161662853+dladbfla@users.noreply.github.com>
2024-07-29 16:15:41 +0900

    7.29

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle } from "styled-components";
-import { MovieShop } from "./Components/MovieShop/MovieShop";
+import { TopDownAction } from "./Components/Unity/TopDownAction";
 
 const GlobalStyle = createGlobalStyle`
 @font-face {
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <MovieShop />
+      <TopDownAction />
     </>
   );
 }
